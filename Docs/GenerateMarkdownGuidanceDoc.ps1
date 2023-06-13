a75c3716021aaa28b6a3559fdfafbb28e671f2f7
Ted Pattison <ted@tedpattison.net>
2023-06-13 11:43:40 -0400

    Updates

--- Docs/GenerateMarkdownGuidanceDoc.ps1
+++
@@ -1,1 +0,0 @@
-pandoc -s --extract-media ./images/BuildingMultiLanguageReportsInPowerBI1 "Building Multi-language Reports in Power BI New.docx" -t gfm -o "Building Multi-language Reports in Power BI New.md"
